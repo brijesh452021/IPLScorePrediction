7094a6397ebd5f236dfe1d7c98e3d7723d66037e
brijesh452021 <81951806+brijesh452021@users.noreply.github.com>
2021-04-14 14:53:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -70,61 +70,61 @@ def predict():
           temp_array = temp_array + [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
         elif(venue=='De Beers Diamond Oval'):
           temp_array = temp_array + [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	      elif(venue=='Dr DY Patil Sports Academy'):
-         temp_array = temp_array + [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] 
-	      elif(venue=='Dr. Y.S. Rajasekhara Reddy ACA-VDCA Cricket Stadium'):
-         temp_array = temp_array + [0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-      	elif(venue=='Dubai International Cricket Stadium'):
-         temp_array = temp_array + [0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] 
-	      elif(venue=='Eden Gardens'):
-         temp_array = temp_array + [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]  
-	      elif(venue=='Feroz Shah Kotla'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	      elif(venue=='Himachal Pradesh Cricket Association Stadium'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	      elif(venue=='Holkar Cricket Stadium'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	      elif(venue=='JSCA International Stadium Complex'):
-         temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]  
-	      elif(venue=='Kingsmead'):
-         temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	      elif(venue=='M Chinnaswamy Stadium'):
-         temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	      elif(venue=='MA Chidambaram Stadium, Chepauk'):
-         temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]   
-	      elif(venue=='Maharashtra Cricket Association Stadium'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	      elif(venue=='New Wanderers Stadium'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	      elif(venue=='Newlands'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0]
-	      elif(venue=='OUTsurance Oval'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0]
-	      elif(venue=='Punjab Cricket Association IS Bindra Stadium, Mohali'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0]
-	      elif(venue=='Punjab Cricket Association Stadium, Mohali'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0]
-	      elif(venue=='Rajiv Gandhi International Stadium, Uppal'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0]
-	      elif(venue=='Sardar Patel Stadium, Motera'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0]
-	      elif(venue=='Sawai Mansingh Stadium'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0]
-	      elif(venue=='Shaheed Veer Narayan Singh International Stadium'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0]
-	      elif(venue=='Sharjah Cricket Stadium'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0]
-	      elif(venue=='Sheikh Zayed Stadium'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0]
-	      elif(venue=='St George's Park'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0]
-	      elif(venue=='Subrata Roy Sahara Stadium'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0]
-	      elif(venue=='SuperSport Park'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
-	      elif(venue=='Wankhede Stadium'):
-	       temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-            
+        elif(venue=='Dr DY Patil Sports Academy'):
+          temp_array = temp_array + [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        elif(venue=='Dr. Y.S. Rajasekhara Reddy ACA-VDCA Cricket Stadium'):
+          temp_array = temp_array + [0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        elif(venue=='Dubai International Cricket Stadium'):
+          temp_array = temp_array + [0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        elif(venue=='Eden Gardens'):
+          temp_array = temp_array + [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        elif(venue=='Feroz Shah Kotla'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        elif(venue=='Himachal Pradesh Cricket Association Stadium'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        elif(venue=='Holkar Cricket Stadium'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        elif(venue=='JSCA International Stadium Complex'):
+          temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        elif(venue=='Kingsmead'):
+          temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        elif(venue=='M Chinnaswamy Stadium'):
+          temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        elif(venue=='MA Chidambaram Stadium, Chepauk'):
+          temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        elif(venue=='Maharashtra Cricket Association Stadium'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        elif(venue=='New Wanderers Stadium'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        elif(venue=='Newlands'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0]
+        elif(venue=='OUTsurance Oval'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0]
+        elif(venue=='Punjab Cricket Association IS Bindra Stadium, Mohali'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0]
+        elif(venue=='Punjab Cricket Association Stadium, Mohali'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0]
+        elif(venue=='Rajiv Gandhi International Stadium, Uppal'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0]
+        elif(venue=='Sardar Patel Stadium, Motera'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0]
+        elif(venue=='Sawai Mansingh Stadium'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0]
+        elif(venue=='Shaheed Veer Narayan Singh International Stadium'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0]
+        elif(venue=='Sharjah Cricket Stadium'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0]
+        elif(venue=='Sheikh Zayed Stadium'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0]
+        elif(venue=='St George'+'s Park'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0]
+        elif(venue=='Subrata Roy Sahara Stadium'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0]
+        elif(venue=='SuperSport Park'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+        elif(venue=='Wankhede Stadium'):
+	        temp_array = temp_array + [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        
             
         overs = float(request.form['overs'])
         runs = int(request.form['runs'])
@@ -132,7 +132,7 @@ def predict():
         runs_in_prev_5 = int(request.form['runs_in_prev_5'])
         wickets_in_prev_5 = int(request.form['wickets_in_prev_5'])
         Batsman =request.form['Batsman']
-        Bowler=request.form['Bowler'])
+        Bowler=request.form['Bowler']
         combo=Batsman + Bowler
 
         data = pd.read_csv('ipl.csv')
@@ -143,13 +143,18 @@ def predict():
           if(Bowler in data_bowlerList):
             if(Runs>=runs_in_prev_5):
               if(wickets_in_prev_5<=wickets):
-        
-        temp_array = temp_array + [overs, runs, wickets, runs_in_prev_5, wickets_in_prev_5,combo]
-        
-        data = np.array([temp_array])
-        my_prediction = int(regressor.predict(data)[0])
-              
-        return render_template('result.html', lower_limit = my_prediction-10, upper_limit = my_prediction+5)
+                temp_array = temp_array + [overs, runs, wickets, runs_in_prev_5, wickets_in_prev_5,combo]
+                data = np.array([temp_array])
+                my_prediction = int(regressor.predict(data)[0])
+                return render_template('result.html', lower_limit = my_prediction-10, upper_limit = my_prediction+5)
+              else:
+                return render_template('index.html',prediction_text="Entered Data is Wrong")
+            else:
+              return render_template('index.html',prediction_text="Entered Data is Wrong")
+          else:
+            return render_template('index.html',prediction_text="Entered Data is Wrong")
+        else:
+          return render_template('index.html',prediction_text="Entered Data is Wrong")
 
 if __name__ == '__main__':
 	app.run(debug=True)
